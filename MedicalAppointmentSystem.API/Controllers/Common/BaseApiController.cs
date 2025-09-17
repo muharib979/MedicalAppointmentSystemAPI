26e5598d3bd49644fb4acc31ed9310f54935a0f5
Muhammad Amran Bhuiyan <muharib979@gmail.com>
2025-09-17 08:35:03 +0600

    Add project files.

---
+++ MedicalAppointmentSystem.API/Controllers/Common/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalAppointmentSystem.API.Controllers.Common
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator _mediatr => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
---
+++ MedicalAppointmentSystem.Infrastructure/DependencyInjection/RepositoriesRegister.cs
@@ -0,0 +1,21 @@
+﻿using MedicalAppointmentSystem.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAppointmentSystem.Infrastructure.DependencyInjection
+{
+    public static class RepositoriesRegister
+    {
+        public static void AddRepositoryServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
